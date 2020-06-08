5fc20b3702e1258a1d9eb08883101dc603d82771
coder2k@mail.ru <standalone!@##@!!@#>
2020-06-08 21:52:31 +0300

    исправлены ошибки с продуктами и добавлением в корзину ajax для удаления из корзины обновление корзины сниппет по удалению

--- cart/urls.py
+++ cart/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from cart.views import cart_home, cart_update, checkout_home, checkout_done_view
+from cart.views import cart_home, cart_update, checkout_home, checkout_done_view, cart_detail_api_view
 
 app_name = "cart"
 
 urlpatterns = [
     path('', cart_home, name='home'),
+    path('api/', cart_detail_api_view, name='api'),
     path('update/', cart_update, name='update'),
     path('checkout/', checkout_home, name='checkout'),
     path('checkout/success/', checkout_done_view, name='success'),
