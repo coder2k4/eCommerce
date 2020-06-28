6d67b4843e8966508d841d79d1b21371878c41b1
Standalone <coder2k@mail.ru>
2020-06-28 15:39:53 +0300

    Расширен views для продуктов Добавлен AJAX для удаления из корзины товара Вынесен сниппет с кнопкой удаления Исправленны незначительные ошибки интерфейса

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from products.views import ProductListView, ProductDetailView
+from products.views import ProductListView, ProductDetailSlugView
 
 app_name = 'Products'
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='list'),
-    path('<str:slug>', ProductDetailView.as_view(), name='detail'),
+    path('<str:slug>', ProductDetailSlugView.as_view(), name='detail'),
 ]
